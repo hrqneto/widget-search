c1921a5f0c7e9c1d4f4742dbc3d93a975eca9b41
Henrique <hrqneto@gmail.com>
2025-02-25 13:01:11 -0300

    build: config vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -4,23 +4,14 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   define: {
-    'process.env': {},
+    'process.env': {}
   },
   build: {
     lib: {
-      entry: '.src/main.tsx',
+      entry: './src/main.tsx',
       name: 'BuscaFlexWidget',
-      formats: ['iife'],
-      fileName: 'embed'
+      fileName: 'embed',
+      formats: ['iife']
     },
-    rollupOptions: {
-      external: ['react', 'react-dom'],
-      output: {
-        globals: {
-          react: 'React',
-          'react-dom': 'ReactDOM'
-        }
-      }
-    }
-  }
+  },
 });
