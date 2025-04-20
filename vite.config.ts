8dd418f0cb72f00f85abaa96cb7a20e91ff8fab8
Henrique <hrqneto@gmail.com>
2025-04-20 13:25:14 -0300

    build(vite): ajuste nas configurações do Rollup para build IIFE e injeção de CSS

--- vite.config.ts
+++ vite.config.ts
@@ -8,19 +8,19 @@ export default defineConfig({
     'process.env': {},
   },
   build: {
+    lib: {
+      entry: './src/widgetConfig/widget.ts',
+      name: 'BuscaFlexWidget',
+      fileName: () => 'embed.iife.js',
+      formats: ['iife'],
+    },
     rollupOptions: {
-      input: './src/widget.tsx',
       output: {
         entryFileNames: 'embed.iife.js',
         assetFileNames: '[name].[ext]',
       },
     },
-    lib: {
-      entry: './src/widget.tsx',
-      name: 'BuscaFlexWidget',
-      fileName: () => 'embed.iife.js',
-      formats: ['iife'],
-    },
-  },
-  
+    outDir: 'dist/widget',
+    emptyOutDir: true,
+  }
 });
