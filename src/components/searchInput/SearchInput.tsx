aa135a786ccdb2bd4a0448af118e759d6d265eff
Henrique <hrqneto@gmail.com>
2025-04-21 19:06:22 -0300

    feat(search-input): adiciona suporte a ref e posicionamento flexível

--- src/components/searchInput/SearchInput.tsx
+++ src/components/searchInput/SearchInput.tsx
@@ -1,15 +1,18 @@
+// 📁 src/components/searchInput/SearchInput.tsx
 import { useEffect } from "react";
 import debounce from "lodash.debounce";
+import { FaSearch } from "react-icons/fa";
 import type { SearchInputProps } from "../../types";
 
 const SearchInput = ({
+  inputRef,
   query,
   setQuery,
   setIsOpen,
   fetchAutocompleteResults,
   fetchSuggestions,
   placeholder = "O que você procura?",
-}: SearchInputProps) => {
+}: SearchInputProps & { inputRef?: React.RefObject<HTMLInputElement | null> }) => {
   useEffect(() => {
     if (query.length >= 1) {
       const debounced = debounce(() => {
@@ -23,28 +26,35 @@ const SearchInput = ({
   }, [query, fetchAutocompleteResults]);
 
   return (
-    <input
-      type="text"
-      autoComplete="off"
-      name="q"
-      className="w-full border rounded-lg p-2.5 mb-2.5 shadow-sm focus:ring-2 focus:ring-blue-400"
-      placeholder={placeholder}
-      value={query}
-      onChange={(e) => {
-        const value = e.target.value;
-        setQuery(value);
-        setIsOpen(true);
-        if (value.length < 1) {
-          fetchSuggestions();
-        }
-      }}
-      onFocus={() => {
-        setIsOpen(true);
-        if (query.length < 1) {
-          fetchSuggestions();
-        }
-      }}
-    />
+    <div className="relative " style={{ width: "200px" }}>
+      <input
+        ref={inputRef}
+        type="text"
+        autoComplete="off"
+        name="q"
+        className="w-full border rounded-lg p-2 pr-10 shadow-sm focus:ring-2 focus:ring-blue-400"
+        placeholder={placeholder}
+        value={query}
+        onChange={(e) => {
+          const value = e.target.value;
+          setQuery(value);
+          setIsOpen(true);
+          if (value.length < 1) {
+            fetchSuggestions();
+          }
+        }}
+        onFocus={() => {
+          setIsOpen(true);
+          if (query.length < 1) {
+            fetchSuggestions();
+          }
+        }}
+      />
+      <FaSearch
+        className="absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-400"
+        size={16}
+      />
+    </div>
   );
 };
 
